baa7c2b99ea1e2467751e7a1757acf8ce21058c0
Pio Molina <vpio@comcast.net>
2019-02-05 18:54:42 -0500

    changed another url for placeholder

--- node/getTodos.js
+++ node/getTodos.js
@@ -1,6 +1,6 @@
 const { request } = require('graphql-request')
 
-const ENDPOINT = `https://api.8base.com/cjrscs00e000y01rt2iesngcu`
+const ENDPOINT = `{your-endpoint-here}`
 
 const GET_TODOS = `
 query {
