8966be543d3a9ffa51ca483749cc6e7ca2f7c757
Pio Molina <vpio@comcast.net>
2019-02-05 18:25:04 -0500

    added makeTodos auth and unauth

---
+++ node/makeTodos.js
@@ -0,0 +1,38 @@
+const { request, GraphQLClient } = require('graphql-request')
+
+// Uncomment the code below to add Authentication
+// const client = new GraphQLClient(ENDPOINT, {
+//   headers: {
+//     Authorization: 'Bearer {your-api-token-here}',
+//   },
+// })
+
+
+const ENDPOINT = `https://api.8base.com/cjrscs00e000y01rt2iesngcu`
+
+const GET_TODOS = `
+query {
+  todosList {
+    items {
+      text
+    }
+  }
+}
+`
+
+const MAKE_TODO = `
+  mutation TodoCreate {
+    todoCreate(
+      data: {
+        text: "from node",
+        completed: false
+    }) {
+      id
+      text
+      completed
+    }
+  }
+`
+
+request(ENDPOINT, MAKE_TODO ).then((r) => console.log(r)) //Use this for unauthenticated
+// client.request(MAKE_TODO).then((r) => console.log(r)) //Use this for authenticated
