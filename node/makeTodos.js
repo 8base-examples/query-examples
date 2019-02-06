900828db784c4c8b939d9f05434826a23a2b288e
Pio Molina <vpio@comcast.net>
2019-02-06 15:41:44 -0500

    add query variables

--- node/makeTodos.js
+++ node/makeTodos.js
@@ -1,6 +1,6 @@
 const { request, GraphQLClient } = require('graphql-request')
 
-const ENDPOINT = `{your-endpoint-here}`
+const ENDPOINT = `https://api.8base.com/cjrscs00e000y01rt2iesngcu`
 
 // Uncomment the code below to add Authentication
 // const client = new GraphQLClient(ENDPOINT, {
@@ -8,6 +8,7 @@ const ENDPOINT = `{your-endpoint-here}`
 //     Authorization: 'Bearer {your-api-token-here}',
 //   },
 // })
+
 
 const GET_TODOS = `
 query {
@@ -20,12 +21,8 @@ query {
 `
 
 const MAKE_TODO = `
-  mutation TodoCreate {
-    todoCreate(
-      data: {
-        text: "from node",
-        completed: false
-    }) {
+  mutation TodoCreate($data: TodoCreateInput!) {
+    todoCreate(data: $data) {
       id
       text
       completed
@@ -33,5 +30,12 @@ const MAKE_TODO = `
   }
 `
 
-request(ENDPOINT, MAKE_TODO ).then((r) => console.log(r)) //Use this for unauthenticated
-// client.request(MAKE_TODO).then((r) => console.log(r)) //Use this for authenticated
+const VARIABLES = {
+  data: {
+    text: "from node",
+    completed: false
+  }
+}
+
+request(ENDPOINT, MAKE_TODO, VARIABLES).then((r) => console.log(r)) //Use this for unauthenticated
+// client.request(MAKE_TODO, VARIABLES).then((r) => console.log(r)) //Use this for authenticated
