d690b12c81ff30d6855979e3a5b7c99775955c51
Pio Molina <41122916+vpio@users.noreply.github.com>
2019-02-06 16:04:59 -0500

    Update js_queries.js

--- js/js_queries.js
+++ js/js_queries.js
@@ -18,7 +18,23 @@ fetch("{your-endpoint-here}",
       "headers": {
           "content-type": "application/json"
       },
-      "body": JSON.stringify({"query": 'mutation TodoCreate($data: TodoCreateInput!) { todoCreate(data: $data) {id, text, completed }}', variables: { data: { text: "from js fetch again", completed: false }  } }),
+      "body": JSON.stringify(
+        {
+          query: `mutation TodoCreate($data: TodoCreateInput!) { 
+                    todoCreate(data: $data) {
+                      id,
+                      text,
+                      completed 
+                    }
+          }`, 
+          variables: { 
+               data: { 
+                 text: "from js fetch again", 
+                 completed: false 
+               }  
+          } 
+         }
+       ),
       "method": "POST"
   }
 ).then((r) => r.json()).then(r => console.log("success: ", JSON.stringify(r)))
@@ -31,7 +47,22 @@ fetch("{your-endpoint-here}",
           "content-type": "application/json",
           "authorization": "Bearer {your-api-token-here}"
       },
-      "body": JSON.stringify({"query": 'mutation TodoCreate($data: TodoCreateInput!) { todoCreate(data: $data) {id, text, completed }}', variables: { data: { text: "from js fetch again", completed: false }  } }),
+      "body": JSON.stringify(
+        {
+          query: `mutation TodoCreate($data: TodoCreateInput!) { 
+                    todoCreate(data: $data) {
+                      id, 
+                      text, 
+                      completed 
+                    }
+          }`, 
+          variables: { 
+            data: { 
+              text: "from js fetch again", 
+              completed: false 
+            }  
+          } 
+        }),
       "method": "POST"
   }
 ).then((r) => r.json()).then(r => console.log("success: ", JSON.stringify(r)))
